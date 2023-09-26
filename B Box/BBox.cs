aa562129c2ee39341c58fda5071a1f4a78395e3c
Charlie Brown <cbrown@brock.ac.uk>
2023-09-26 14:19:51 +0100

    26th sep

--- B Box/BBox.cs
+++ B Box/BBox.cs
@@ -8,17 +8,39 @@ namespace B_Box
 {
     internal class BBox
     {
+
+        static void PrintList(int num)
+        {
+           // counter++;
+            num = num - 1;
+            //if (num > 1)
+            {
+                //Console.WriteLine(counter);
+                PrintList(num);
+            }
+            Console.WriteLine(num);
+        }
         static void Main(string[] args)
         {
-            Stack <string> stack = new Stack<string>();
-            stack.Push("ray");
-            stack.Push("zoe");
-            stack.Push("jon");
-            stack.Push("mik");
-            string item = stack.Pop();
-            Console.WriteLine(item);
+            PrintList(10);
+            Console.WriteLine("finished");
+            int x = 99;
+            Fred();
+            Console.WriteLine(x);
             Console.ReadLine();
 
+        }
+
+        private static void Fred()
+        {
+            int q = 56;
+            Jeff();
+            Console.WriteLine(q);
+        }
+
+        private static void Jeff()
+        {
+            Console.WriteLine("hello from jeff");
         }
     }
 }
---
+++ RecusionExample-FractalTree/RecusionExample-FractalTree/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecusionExample_FractalTree
+{
+    public partial class Form1 : Form
+    {
+        static Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.LightCyan, 1);
+            WindowState = FormWindowState.Maximized;
+            //this.BackColor = Color.Black;
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            SimpleTree(e);
+        }
+
+        private void SimpleTree(PaintEventArgs e)
+        {
+            
+            float len = 300;
+            e.Graphics.TranslateTransform(this.Width / 2, this.Height);
+            Branch(e, len, Color.BlueViolet);
+        }
+
+        private void Branch(PaintEventArgs e, float len, Color treeColor)
+
+        {
+            float theata;
+            Pen pen = new Pen(Brushes.LimeGreen);
+            pen.Color = treeColor;
+            pen.Width = len * 1 / 20;
+            GraphicsState graphicsState;
+            e.Graphics.DrawLine(pen, 0, 0, 0, -len);
+            pen.Width = pen.Width * 0.66F;
+            e.Graphics.TranslateTransform(0, -len);
+            len = len * 0.66F;
+            if (len > 2.0F)
+            {
+
+                graphicsState = e.Graphics.Save();
+                theata = rnd.Next(0, 60);
+                e.Graphics.RotateTransform(theata);
+                System.Threading.Thread.Sleep(500);
+                Branch(e, len, treeColor);
+                e.Graphics.Restore(graphicsState);
+                graphicsState = e.Graphics.Save();
+                theata = rnd.Next(10, 60);
+                e.Graphics.RotateTransform(-theata);
+                System.Threading.Thread.Sleep(500);
+                Branch(e, len, treeColor);
+                e.Graphics.Restore(graphicsState);
+
+            }
+        }
+    }
+}
