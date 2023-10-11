7415c0a175a8a3b7196ac6acc282e34ebc663e64
Charlie Brown <CBROWN@brock.ac.uk>
2023-10-11 12:05:28 +0100

    test

--- UserControlExample/Form1.Designer.cs
+++ UserControlExample/Form1.Designer.cs
@@ -55,7 +55,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(796, 64);
+            this.button1.Location = new System.Drawing.Point(803, 153);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(175, 131);
             this.button1.TabIndex = 2;
