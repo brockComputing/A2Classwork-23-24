9e36385dba469b98edce102be27c31b56c5fea30
Charlie Brown <cbrown@brock.ac.uk>
2023-10-31 10:31:06 +0000

    Moved button

--- UserControlExample/Form1.Designer.cs
+++ UserControlExample/Form1.Designer.cs
@@ -36,41 +36,41 @@
             // ucTest1
             // 
             this.ucTest1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.ucTest1.Location = new System.Drawing.Point(410, 212);
-            this.ucTest1.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.ucTest1.Location = new System.Drawing.Point(205, 110);
+            this.ucTest1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.ucTest1.Name = "ucTest1";
             this.ucTest1.numberofLates = 0;
-            this.ucTest1.Size = new System.Drawing.Size(300, 288);
+            this.ucTest1.Size = new System.Drawing.Size(150, 150);
             this.ucTest1.TabIndex = 0;
             // 
             // ucTest2
             // 
             this.ucTest2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.ucTest2.Location = new System.Drawing.Point(1026, 244);
-            this.ucTest2.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.ucTest2.Location = new System.Drawing.Point(513, 127);
+            this.ucTest2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.ucTest2.Name = "ucTest2";
             this.ucTest2.numberofLates = 0;
-            this.ucTest2.Size = new System.Drawing.Size(300, 288);
+            this.ucTest2.Size = new System.Drawing.Size(150, 150);
             this.ucTest2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(803, 153);
+            this.button1.Location = new System.Drawing.Point(400, 155);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(175, 131);
+            this.button1.Size = new System.Drawing.Size(88, 68);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.ucTest2);
             this.Controls.Add(this.ucTest1);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
