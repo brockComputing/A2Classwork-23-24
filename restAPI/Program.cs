616689c6c5cb00a5b168e737cf40ad3448599e76
Charlie Brown <CBROWN@brock.ac.uk>
2024-01-10 16:18:41 +0000

    added rest API examples

---
+++ restAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://cat-fact.herokuapp.com/facts/");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+            Console.ReadLine();
+
+        }
+    }
+}
