e590d6e8df267bb45d4d44a52d77e7df2e3c5574
Charlie Brown <CBROWN@brock.ac.uk>
2024-04-09 11:56:25 +0100

    Revert "6B"
    This reverts commit da21de68253adb247bd15abefd60a4398a050f4b.

--- SymbolPuzzleB/Q6B.cs
+++ SymbolPuzzleB/Q6B.cs
@@ -218,7 +218,7 @@ namespace Puzzle
                     // allowed patterns
                     foreach (var pattern in AllowedPatterns)
                     {
-                        writer.WriteLine(pattern.GetPatternSequence());
+                        writer.WriteLine(pattern.GetPatternSequence()); 
                     }
                     writer.WriteLine(GridSize);
                     // the cells
@@ -226,12 +226,8 @@ namespace Puzzle
                     foreach (var cell in Grid)
                     {
                         cellLine = cell.GetSymbolForSave() + ",";
-                        cellLine += cell.GetSymbolsNotAllowedForSave();
-                        writer.WriteLine(cellLine);
-                        cellLine = "";
+
                     }
-                    writer.WriteLine(Score.ToString());
-                    writer.WriteLine(SymbolsLeft.ToString());
                 }
             }
 
@@ -381,17 +377,11 @@ namespace Puzzle
             {
                 // return SymbolsNotAllowed seperated by commas
                 string value = "";
-                foreach (var symbol in SymbolsNotAllowed)
-                {
-                    value = value + symbol + ",";
-                }
-                if (value.Length > 0)
-                {
-                    value = value.Remove(value.Length - 1); // removes the last comma
-                }
+
+
                 return value;
             }
-
+            
             public virtual string GetSymbolForSave()
             {
                 return Symbol;
