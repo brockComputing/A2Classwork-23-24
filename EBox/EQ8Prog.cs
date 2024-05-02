e2adbdd27c1856f0f4c64b13f8633113fe356a6b
Charlie Brown <cbrown@brock.ac.uk>
2024-05-02 13:47:23 +0100

    E started

---
+++ EBox/EQ8Prog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBox
+{
+    internal class EQ8Prog
+    {
+        static void Main(string[] args)
+        {
+            bool valid;
+            do
+            {
+                valid = true;
+
+            } while (valid == false) ;
+            Console.WriteLine("its valid");
+            Console.ReadLine();
+        }
+    }
+}
