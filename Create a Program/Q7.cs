2c8c6225f7b1240e75272d8c38649ece5b390cc9
Charlie Brown <CBROWN@brock.ac.uk>
2024-05-02 15:34:03 +0100

    Q7 create a program

---
+++ Create a Program/Q7.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_a_Program
+{
+    internal class Q7
+    {
+        static void Main(string[] args)
+        {
+            int startPos = 0, endPos = 0;
+            Console.WriteLine("Enter the word");
+            string theWord = Console.ReadLine();
+            string vowels = "aeiou";
+            endPos = theWord.Length - 1;
+            while (startPos < endPos)
+            {
+                if (vowels.Contains(theWord[startPos].ToString()))
+                {
+                    // find the vowel
+                    bool foundEndVowel = false;
+                    do
+                    {
+                        if (vowels.Contains(theWord[endPos].ToString()))
+                        {
+                            foundEndVowel = true;
+                            break;
+                        }
+                        endPos--;
+                    } while (foundEndVowel == false && startPos < endPos);
+                    //swap
+                    if (foundEndVowel)
+                    {
+                        string first = theWord[startPos].ToString();
+                        string second = theWord[endPos].ToString();
+                        theWord = theWord.Insert(startPos, second);
+                        theWord = theWord.Remove(startPos + 1,1);
+                        theWord = theWord.Insert(endPos, first);
+                        theWord = theWord.Remove(endPos + 1,1);
+                        endPos--;
+                    }
+                }
+                startPos++;
+            }
+            Console.WriteLine(theWord);
+            Console.ReadLine();
+
+        }
+    }
+}
