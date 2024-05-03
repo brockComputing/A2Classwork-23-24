c771c10929256b1d022a4383c800f3ba8c1ee615
Charlie Brown <cbrown@brock.ac.uk>
2024-05-03 14:15:21 +0100

    MORE

---
+++ A box/AQ7Prog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Q8AGroup
+{
+    internal class Q8A
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Console.ReadLine();
+        }
+    }
+}
--- A box/AQ8Prog.cs
+++ A box/AQ8Prog.cs
@@ -1,36 +1,65 @@
-﻿using System;
+﻿//using System;
 
-namespace Q8AGroup
-{
-    internal class Q8A
-    {
-        
-        static void Main(string[] args)
-        {
-            bool valid;
+//namespace Q8AGroup
+//{
+//    internal class Q8A
+//    {
 
-            do
-            {
-                Console.WriteLine("enter a string");
-                string theString = Console.ReadLine();
-                valid = true;
-                if (theString.Length < 5 || theString.Length > 7)
-                {
-                    valid = false;
-                    Console.WriteLine("not correct number of  chars");
-                }
-                // check if uppercase
-                string copyOF = theString;
-                if (copyOF.ToUpper() != theString)
-                {
-                    valid = false;
-                    Console.WriteLine();
-                }
+//        static void Main(string[] args)
+//        {
+//            bool valid;
 
-            } while (valid == false);
-            Console.WriteLine("valid string");
-            Console.ReadLine();
-            Console.ReadLine();
-        }
-    }
-}
+//            do
+//            {
+//                Console.WriteLine("enter a string");
+//                string theString = Console.ReadLine();
+//                valid = true;
+//                if (theString.Length < 5 || theString.Length > 7)
+//                {
+//                    valid = false;
+//                    Console.WriteLine("not correct number of  chars");
+//                }
+//                check if uppercase
+//                string copyOF = theString;
+//                if (copyOF.ToUpper() != theString)
+//                {
+//                    valid = false;
+//                    Console.WriteLine("not upper case");
+//                }
+//                duplicates
+//                for (int pos = 0; pos < theString.Length; pos++)
+//                {
+//                    int count = 0;
+//                    string currentLetter = theString[pos].ToString();
+//                    for (int i = 0; i < theString.Length; i++)
+//                    {
+//                        if (currentLetter == theString[i].ToString())
+//                        {
+//                            count++;
+//                        }
+//                    }
+//                    if (count > 1)
+//                    {
+//                        Console.WriteLine("duplicates");
+//                        valid = false;
+//                        break;
+//                    }
+//                }
+//                ascii codes
+//                int asciiTotal = 0;
+//                for (int i = 0; i < theString.Length; i++)
+//                {
+//                    asciiTotal = asciiTotal + theString[i];
+//                }
+//                if (asciiTotal < 420 || asciiTotal > 600)
+//                {
+//                    Console.WriteLine("Ascii not in range");
+//                    valid = false;
+//                }
+//            } while (valid == false);
+//            Console.WriteLine("valid string");
+//            Console.ReadLine();
+//            Console.ReadLine();
+//        }
+//    }
+//}
